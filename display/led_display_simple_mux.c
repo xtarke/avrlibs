127d04a4c21c69055ae17df9f0d73eb198a918de
Renan Augusto Starke <xtarke@gmail.com>
2019-08-26 14:43:37 -0300

    Correct display MUX

--- display/led_display_simple_mux.c
+++ display/led_display_simple_mux.c
@@ -15,7 +15,7 @@
 /* Esqueleto da estrutura para guardar as configurações */
 typedef struct displays {
 	uint8_t total;
-	uint8_t mask;
+	uint8_t atual;
 } displays_t;
 
 /* Instância da struct para manter as configurações */
@@ -33,17 +33,13 @@ const uint8_t convTable[] PROGMEM = { ...  };
 
 void displaysInit(uint8_t displays){
 
-	uint8_t mask = (2 << displays) - 1;
-
 	/* Variáveis de controle */
 	displays_info.total = displays;
-	displays_info.mask = mask;
 
 	/* Todos os pinos como saída */
-	DISPLAY_DATA_PORT->DDR = 0xff;
-	DISPLAY_MUX_PORT->DDR = mask;
+	// DISPLAY_DATA_PORT->DDR = 
+	// DISPLAY_MUX_PORT->DDR = 
 
-	displays_info.mask = ~mask;
 }
 
 
@@ -70,7 +66,7 @@ void displaysWrite(uint8_t hexValue){
 		hexValue = hexValue >> 4;
 		_delay_ms(10);
 
-		/* Desliga os displays */
-		DISPLAY_MUX_PORT->PORT &=  displays_info.mask;
+		/* Desligar os displays */
+		// >> complete aqui
 	}
 }
